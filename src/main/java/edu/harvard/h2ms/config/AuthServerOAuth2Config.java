63029411f63746107f31fdad6761ccbe8d620b3c
shinwachi <shinwachi@gmail.com>
2018-03-21 17:16:26 -0400

    unbroke it for now

--- src/main/java/edu/harvard/h2ms/config/AuthServerOAuth2Config.java
+++ src/main/java/edu/harvard/h2ms/config/AuthServerOAuth2Config.java
@@ -30,74 +30,74 @@ import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 //@PropertySource({ "classpath:persistence.properties" })
 //@EnableAuthorizationServer
 public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter{
-
-	@Autowired
-	private Environment env;
-	
-	
-	@Autowired
-	@Qualifier("authenticationManagerBean")
-	private AuthenticationManager authenticationManager;
-
-	@Value("classpath:schema.sql")
-	private Resource schemaScript;
-	
-	@Value("classpath:data.sql")
-	private Resource dataScript;
-	
-	@Override
-	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception{
-		oauthServer
-		.tokenKeyAccess("permitAll()")
-		.checkTokenAccess("isAuthenticated()");
-	}
-	
-	@Override
-	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.jdbc(dataSource())
-			.withClient("sampleClientId")
-			.authorizedGrantTypes("implicit")
-			.scopes("read")
-			.autoApprove(true)
-			.and()
-			.withClient("clientIdPassword")
-			.secret("secret")
-			.authorizedGrantTypes(
-					"password", "authorization_code", "refresh_token")
-			.scopes("read");
-			
-	}
-	
-	
-
-	@Bean
-	public DataSourceInitializer datasourceInitializer (DataSource dataSource) {
-		DataSourceInitializer initializer = new DataSourceInitializer();
-		initializer.setDataSource(dataSource);
-		initializer.setDatabasePopulator(databasePopulator());
-		return initializer;
-	}
-	
-	private DatabasePopulator databasePopulator() {
-		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-		populator.addScript(schemaScript);
-		return populator;
-	}
-	
-	@Bean
-	public DataSource dataSource() {
-		DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
-		dataSource.setUrl(env.getProperty("jdbc.url"));
-		dataSource.setUsername(env.getProperty("jdbc.user"));
-		dataSource.setPassword(env.getProperty("jdbc.pass"));
-		return dataSource;
-	}
-	
-	@Bean
-	public TokenStore tokenStore() {
-		return new JdbcTokenStore(dataSource());
-	}
-	
+//
+//	@Autowired
+//	private Environment env;
+//	
+//	
+//	@Autowired
+//	@Qualifier("authenticationManagerBean")
+//	private AuthenticationManager authenticationManager;
+//
+//	@Value("classpath:schema.sql")
+//	private Resource schemaScript;
+//	
+//	@Value("classpath:data.sql")
+//	private Resource dataScript;
+//	
+//	@Override
+//	public void configure(AuthorizationServerSecurityConfigurer oauthServer) throws Exception{
+//		oauthServer
+//		.tokenKeyAccess("permitAll()")
+//		.checkTokenAccess("isAuthenticated()");
+//	}
+//	
+//	@Override
+//	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//		clients.jdbc(dataSource())
+//			.withClient("sampleClientId")
+//			.authorizedGrantTypes("implicit")
+//			.scopes("read")
+//			.autoApprove(true)
+//			.and()
+//			.withClient("clientIdPassword")
+//			.secret("secret")
+//			.authorizedGrantTypes(
+//					"password", "authorization_code", "refresh_token")
+//			.scopes("read");
+//			
+//	}
+//	
+//	
+//
+//	@Bean
+//	public DataSourceInitializer datasourceInitializer (DataSource dataSource) {
+//		DataSourceInitializer initializer = new DataSourceInitializer();
+//		initializer.setDataSource(dataSource);
+//		initializer.setDatabasePopulator(databasePopulator());
+//		return initializer;
+//	}
+//	
+//	private DatabasePopulator databasePopulator() {
+//		ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
+//		populator.addScript(schemaScript);
+//		return populator;
+//	}
+//	
+//	@Bean
+//	public DataSource dataSource() {
+//		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+//		dataSource.setUrl(env.getProperty("jdbc.url"));
+//		dataSource.setUsername(env.getProperty("jdbc.user"));
+//		dataSource.setPassword(env.getProperty("jdbc.pass"));
+//		return dataSource;
+//	}
+//	
+//	@Bean
+//	public TokenStore tokenStore() {
+//		return new JdbcTokenStore(dataSource());
+//	}
+//	
 	
 }
