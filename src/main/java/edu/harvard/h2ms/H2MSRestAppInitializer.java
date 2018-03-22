a95759702e9013ab6fffec2c7fd0e5e252d958e4
shinwachi <shinwachi@gmail.com>
2018-03-22 19:32:25 -0400

    removed the swagger annot.

--- src/main/java/edu/harvard/h2ms/H2MSRestAppInitializer.java
+++ src/main/java/edu/harvard/h2ms/H2MSRestAppInitializer.java
@@ -5,20 +5,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * H2MS Rest Application
  * @version 1.0
  */
 @SpringBootApplication
-@EnableSwagger2
 @EnableResourceServer
-@Import(SpringDataRestConfiguration.class)
 public class H2MSRestAppInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(H2MSRestAppInitializer.class);
